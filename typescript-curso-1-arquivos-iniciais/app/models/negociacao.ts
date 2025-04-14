84a527b09c5794dbb3aebfd07f2a13cf22d73386
SilvioClaudioJr <silvioclaudiojr10@gmail.com>
2025-04-14 14:35:20 -0300

    commit

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,9 +1,9 @@
 export class Negociacao {
-    private _data;
-    private _quantidade;
-    private _valor;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
-    constructor(data, quantidade, valor) {
+    constructor(data: Date, quantidade:number, valor: number) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
@@ -17,7 +17,7 @@ export class Negociacao {
         return this._quantidade;
     }
 
-    get valor () {
+get valor () {
         return this._valor;
     }
 
